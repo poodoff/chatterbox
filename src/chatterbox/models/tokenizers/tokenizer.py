8f9ea7a38ab5a05862aba3fd3be81517bf2cb833
poodoff <workonfx@gmail.com>
2026-06-22 21:17:06 +0300

    Update tokenizer.py rollback EnTokenizer class

--- src/chatterbox/models/tokenizers/tokenizer.py
+++ src/chatterbox/models/tokenizers/tokenizer.py
@@ -31,6 +31,40 @@ RUSSIAN_STRESSER_INSTALL_DIR = Path.home() / ".cache" / "chatterbox" / "russian-
 _russian_stresser_lock = threading.Lock()
 _russian_stresser_local = threading.local()
 
+class EnTokenizer:
+    def __init__(self, vocab_file_path):
+        self.tokenizer: Tokenizer = Tokenizer.from_file(vocab_file_path)
+        self.check_vocabset_sot_eot()
+
+    def check_vocabset_sot_eot(self):
+        voc = self.tokenizer.get_vocab()
+        assert SOT in voc
+        assert EOT in voc
+
+    def text_to_tokens(self, text: str):
+        text_tokens = self.encode(text)
+        text_tokens = torch.IntTensor(text_tokens).unsqueeze(0)
+        return text_tokens
+
+    def encode(self, txt: str):
+        """
+        clean_text > (append `lang_id`) > replace SPACE > encode text using Tokenizer
+        """
+        txt = txt.replace(' ', SPACE)
+        code = self.tokenizer.encode(txt)
+        ids = code.ids
+        return ids
+
+    def decode(self, seq):
+        if isinstance(seq, torch.Tensor):
+            seq = seq.cpu().numpy()
+
+        txt: str = self.tokenizer.decode(seq, skip_special_tokens=False)
+        txt = txt.replace(' ', '')
+        txt = txt.replace(SPACE, ' ')
+        txt = txt.replace(EOT, '')
+        txt = txt.replace(UNK, '')
+        return txt
 
 class ChineseCangjieConverter:
     """Convert Chinese characters to Cangjie tokens used by the model."""
