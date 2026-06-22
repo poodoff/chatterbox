cbf1d5387c42fd264872e46f079731e5cfe7fe30
poodoff <workonfx@gmail.com>
2026-06-22 13:20:08 +0300

    Update t3_config.py adapted from ResembleAI/Chatterbox-Multilingual-TTS-V3

--- src/chatterbox/models/t3/modules/t3_config.py
+++ src/chatterbox/models/t3/modules/t3_config.py
@@ -39,3 +39,30 @@ class T3Config:
     def multilingual(cls):
         """Create configuration for multilingual TTS model."""
         return cls(text_tokens_dict_size=2454)
+
+class T3ConfigMultilingual:
+    text_preproc = "NFKD,fullcase"
+
+    start_text_token = 255
+    stop_text_token = 0
+    text_tokens_dict_size = 2352
+    max_text_tokens = 2048
+
+    start_speech_token = 6561
+    stop_speech_token = 6562
+    speech_tokens_dict_size = 8194
+    max_speech_tokens = 4096
+
+    llama_config_name = "Llama_520M"
+    input_pos_emb = "learned"
+    speech_cond_prompt_len = 150
+
+    # For T3CondEnc
+    encoder_type = "voice_encoder"
+    speaker_embed_size = 256
+    use_perceiver_resampler = True
+    emotion_adv = True
+
+    @property
+    def n_channels(self):
+        return LLAMA_CONFIGS[self.llama_config_name]["hidden_size"]
